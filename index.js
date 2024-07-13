37a116dd0c28dc84822ce736998c636a75ff8d1b
NForlini <ninaforlini1@gmail.com>
2024-07-13 00:31:37 -0400

    Create index.js
    created on VSCode

---
+++ index.js
@@ -0,0 +1,28 @@
+const buttonsEl = document.querySelectorAll("button");
+const inputEl = document.getElementById("result");
+
+for (let i = 0; i < buttonsEl.length; i++){
+    buttonsEl[i].addEventListener("click",()=>{
+        const buttonValue = buttonsEl[i].textContent;
+        if(buttonValue === "C"){
+            clearResults()
+        } else if (buttonValue === "="){
+            calculateResults()
+        } else{
+            appendValue(buttonValue);
+        }
+    });
+}
+
+function clearResults(){
+    inputEl.value = ""
+};
+
+function calculateResults(){
+    inputEl.value = eval(inputEl.value);
+};
+
+function appendValue(buttonValue){
+    inputEl.value += buttonValue
+    // inputEl.value = inputEl.value + buttonValue;
+};
